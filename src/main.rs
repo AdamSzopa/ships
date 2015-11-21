1f347c61974b3e0d29e47e83b110830ac455c196
Adam Szopa <adam@szopa.org.pl>
2015-11-21 21:24:03 +0100

    Make the loop a bit nicer

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,8 @@ fn main() {
     let mut map = [[Field::Empty;10];10];
     map[3][2] = Field::Hit{ship: Ship{length:5,life_left:5}};
 
-    for (i,_) in map.iter().enumerate(){
-        for inner in &map[i]{
+    for outer in map.iter(){
+        for inner in outer{
             match *inner {
                 Field::Empty => print!(". "),
                 Field::Hit{ship} => print!("{} ",ship.life_left)

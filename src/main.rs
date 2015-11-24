5c0ce39fdabdaf043c655dd834d5d0a0666c5a6d
Adam Szopa <adam@szopa.org.pl>
2015-11-24 20:58:53 +0100

    commit before big change

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,60 @@
+use std::rc::Rc;
+
 #[derive (Copy,Clone)]
 struct Ship{
     length: i8,
     life_left: i8,
 }
 
+impl Ship{
+    fn hit(&mut self){
+        self.life_left -= 1;
+    }
+}
+
 #[derive (Copy,Clone)]
 enum Field{
     Empty,
-    Hit{ship :Ship},
+    Hit{ship: Ship},
+}
+
+enum Field2{
+    Empty,
+    Hit{ship: Rc<Ship>},
 }
 
 fn main() {
     let mut map = [[Field::Empty;10];10];
-    map[3][2] = Field::Hit{ship: Ship{length:5,life_left:5}};
 
-    for outer in map.iter(){
-        for inner in outer{
-            match *inner {
-                Field::Empty => print!(". "),
-                Field::Hit{ship} => print!("{} ",ship.life_left)
+    let mut ship7 = Ship{length:7,life_left:7};
+    let mut map2 = [[Field2::Empty,Field2::Hit{ship: Rc::new(ship7)},Field2::Empty],[Field2::Empty,Field2::Empty,Field2::Empty]];
+
+    let ship5 = Ship{length:5,life_left:5};
+    map[3][2] = Field::Hit{ship: ship5};
+    map[3][3] = Field::Hit{ship: ship5};
+
+        for outer in map.iter_mut(){
+            for inner in outer.iter_mut(){
+                match *inner {
+                    Field::Empty => print!(". "),
+                    Field::Hit{ref mut ship} => {
+                        ship.hit();
+                        print!("{} ",ship.life_left);
+                    }
+                }
             }
+            println!("");
         }
-        println!("");
-    }
+        for outer in map2.iter_mut(){
+            for inner in outer.iter_mut(){
+                match *inner {
+                    Field2::Empty => print!(". "),
+                    Field2::Hit{ref mut ship} => {
+                        //ship.hit();
+                        print!("{} ",ship.life_left);
+                    }
+                }
+            }
+            println!("");
+        }
 }
